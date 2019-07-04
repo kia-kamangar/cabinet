5f1532efb13586c62e925e189bcc89e4855ac4ca
kia <kia.kamangar@gmail.com>
2019-07-04 12:37:26 +0430

    Add relation between user and doc

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_04_072030) do
+ActiveRecord::Schema.define(version: 2019_07_04_073329) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
